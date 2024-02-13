649068056e8dd97b76a8776b8bd7cecb37fafe73
kailam-ada <kai_lam@hotmail.fr>
2024-02-13 13:41:54 +0100

    Login and Register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,20 +5,26 @@ import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
 import axios from 'axios';
+import { UserContextProvider } from './UserContext';
 
 axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.withCredentials = true;
 
 function App() {
 
   return (
+    <UserContextProvider>
     <Routes>
       {/* not closed to put content inside */}
       <Route path="/" element={<Layout />} > 
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage />}/>
         <Route path="/register" element={<RegisterPage />}/>
+        <Route path="/account" element={<AccountPage />} />
       </Route >
     </Routes>
+
+    </UserContextProvider>
   )
 }
 
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -6,9 +6,18 @@ export default function RegisterPage() {
     const [name,setName] = useState('');
     const [email,setEmail] = useState('');
     const [password,setPassword] = useState('');
-    const registerUser = (ev) => {
+    async function registerUser(ev) {
         ev.preventDefault();
-        axios.get('/test');
+        try {
+            await axios.post('/register', {
+                name,
+                email,
+                password,
+            });
+            alert('Registration successful. Now you can log in.')
+        } catch (e) {
+            alert('Registration failed. Please try again later')
+        }
     }
  
     return (
