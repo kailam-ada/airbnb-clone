94c0a44ff4a41b0e187d1a1b3eceddc9773fd971
kailam-ada <kai_lam@hotmail.fr>
2024-02-13 15:58:32 +0100

    Account Page set

--- api/index.js
+++ api/index.js
@@ -67,9 +67,9 @@ app.post('/login', async (req,res) => {
 });
 
 app.get('/profile', (req,res) => {
-    const {token} = req.cookie;
+    const {token} = req.cookies;
     if (token) {
-        jwt.verify(token,jwtSecret, {}, async (err,user) => {
+        jwt.verify(token, jwtSecret, {}, async (err, userData) => {
             if (err) throw err;
             const {name,email,_id} = await User.findById(userData.id);
             res.json({name,email,_id});
@@ -78,5 +78,9 @@ app.get('/profile', (req,res) => {
         res.json(null)
     }
 });
+
+app.post('/logout', (req,res) => {
+    res.cookie('token', '').json(true);
+})
 
 app.listen(4000);
