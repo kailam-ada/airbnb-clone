26455071a06ce61b6128f6ccb1993fcb169687da
kailam-ada <kai_lam@hotmail.fr>
2024-02-13 13:56:20 +0100

    set up Account Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext';
+import AccountPage from './pages/AccountPage';
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
