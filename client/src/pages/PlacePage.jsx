071c9bad7456a2f72f6cb8f5ea05e02d9e9ed6da
kailam-ada <kai_lam@hotmail.fr>
2024-02-22 16:12:49 +0100

    refacto

--- client/src/pages/PlacePage.jsx
+++ client/src/pages/PlacePage.jsx
@@ -8,7 +8,6 @@ import AddressLink from "../AddressLink";
 export default function PlacePage() {
     const {id} = useParams();
     const [place,setPlace] = useState(null);
-    const [showAllPhotos,setShowAllPhotos] = useState(false);
     useEffect(() => {
         if (!id) {
             return;
@@ -20,28 +19,6 @@ export default function PlacePage() {
 
     if (!place) return '';
 
-    if (showAllPhotos) {
-        return (
-            <div className="absolute inset-0 bg-black text-white min-h-screen">
-                <div className="bg-black p-8 grid gap-4">
-                    <div>
-                        <h2 className="text-3xl mr-48">Photos of {place.title}</h2>
-                        <button onClick={() => setShowAllPhotos(false)} className="fixed right-12 top-8 flex gap-1 py-2 px-4 rounded-2xl shadow shadow-black bg-white text-black">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
-                            <path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                        </svg>
-                            Close photos
-                        </button>
-                    </div>
-                    {place?.photos?.length > 0 && place.photos.map(photo => (
-                        <div key={place._id}>
-                            <img src={'http://localhost:4000/uploads/'+photo} alt="" />
-                        </div>
-                    ))}
-                </div>
-            </div>
-        )
-    }
 
     return (
         <div className="mt-4 bg-gray-100 -mx-8 px-8 pt-8">
@@ -69,5 +46,5 @@ export default function PlacePage() {
                 <div className="mb-4 mt-2 text-sm text-gray-700 leading-5">{place.extraInfo}</div>
             </div>
         </div>
-    )
+    );
 }
